f10cbe2659dbe2b3354b5e98e294254a57530608
혜민 <97522726+heymin2@users.noreply.github.com>
2023-09-22 13:28:41 +0900

    Feat: 백그라운드, 포어그라운드 fcm 설정 추가

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -2,9 +2,21 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import MainStack from './MainStack';
 import AuthStack from './AuthStack';
+import React, {useEffect} from 'react';
+import messaging from '@react-native-firebase/messaging';
+
 const RootStack = createStackNavigator();
 
 const Navigation = () => {
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      console.log('foreground:', remoteMessage);
+      // 여기에서 메시지를 처리하고 화면에 표시할 수 있습니다.
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <NavigationContainer>
       <RootStack.Navigator
