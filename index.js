1d00d410264439b9216093f899d7c8344d36ef2d
혜민 <alddlv81@gmail.com>
2023-10-11 21:03:13 +0900

    Feat: 백그라운드 token 설정

--- index.js
+++ index.js
@@ -6,9 +6,34 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import messaging from '@react-native-firebase/messaging';
+import {url} from './src/url';
 
 messaging().setBackgroundMessageHandler(async remoteMessage => {
   console.log('background: ', remoteMessage);
+  const call = remoteMessage.data.call;
+  if (call == 'incoming') {
+    fcmget();
+  }
 });
+
+const fcmget = async () => {
+  const token = await messaging().getToken();
+  await fetch(`${url}/twilio/token`, {
+    method: 'POST',
+    body: JSON.stringify({
+      fcmToken: token,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log('성공', data);
+    })
+    .catch(error => {
+      console.error('실패', error);
+    });
+};
 
 AppRegistry.registerComponent(appName, () => App);
