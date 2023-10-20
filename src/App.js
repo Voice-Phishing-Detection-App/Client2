9de7b46af886baa6f3601cfe9f2296e159e6dfce
혜민 <alddlv81@gmail.com>
2023-10-20 16:56:39 +0900

    Fix: 앱 초기화면

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
-// import {UserProvider} from './contexts/UserContext';
-// import Navigation from './navigations';
-
-// const App = () => {
-//   return (
-//     <UserProvider>
-//       <Navigation />
-//     </UserProvider>
-//   );
-// };
-
-// export default App;
-
-import MainStack from './navigations/MainStack';
-import AuthStack from './navigations/AuthStack';
-import {NavigationContainer} from '@react-navigation/native';
-import File from './test/File';
+import {UserProvider} from './contexts/UserContext';
+import Navigation from './navigations';
 
 const App = () => {
   return (
-    // <NavigationContainer>
-    //   <MainStack />
-    // </NavigationContainer>
-    <>
-      <File />
-    </>
+    <UserProvider>
+      <Navigation />
+    </UserProvider>
   );
 };
 
