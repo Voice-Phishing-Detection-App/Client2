93dfe552f32f93698f02a95324a415e01def659a
moonyeju <moonyeju8510@gmail.com>
2023-11-02 00:02:39 +0900

    FEAT: m4a file 통신코드 추가

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -3,59 +3,61 @@ import {createStackNavigator} from '@react-navigation/stack';
 import MainStack from './MainStack';
 import AuthStack from './AuthStack';
 import messaging from '@react-native-firebase/messaging';
-// import SInfo from 'react-native-sensitive-info';
-// import {url} from '../url';
 import React, {useEffect, useState} from 'react';
-// import {PERMISSIONS, request} from 'react-native-permissions';
 import {PermissionsAndroid} from 'react-native';
 import CallDetectorManager from 'react-native-call-detection';
 import RNFetchBlob from 'rn-fetch-blob'; // rn-fetch-blob 라이브러리 추가
 import {check, request, PERMISSIONS, RESULTS} from 'react-native-permissions';
+import {url} from '../url';
 const RootStack = createStackNavigator();
 
 const Navigation = () => {
-  // const [incoming, setIncoming] = useState(null);
-  // const [number, setNumber] = useState(null);
   const directoryPath = `${
     Platform.OS === 'android'
       ? RNFetchBlob.fs.dirs.SDCardDir // Android의 내장 메모리 경로
       : RNFetchBlob.fs.dirs.DocumentDir // iOS 또는 다른 플랫폼의 내장 메모리 경로
   }/Call/`;
   const [mostRecentFile, setMostRecentFile] = useState(null);
-  // const [twilioToken, setTwilioToken] = useState(null);
-  // const [incomingCall, setIncomingCall] = useState(false);
+
+  const formData = new FormData();
+  formData.append('audio', {
+    uri: mostRecentFile,
+    type: 'audio/x-m4a',
+    name: 'audio.m4a',
+  });
+
   useEffect(() => {
-    requestMicrophonePermission();
     const unsubscribe = messaging().onMessage(async remoteMessage => {
       console.log('Foreground Message:', remoteMessage);
 
       if (remoteMessage.data.call === 'incoming') {
         console.log('call', remoteMessage.data);
-        // fcmget();
       }
     });
     return unsubscribe;
   }, []);
 
-  const requestMicrophonePermission = async () => {
-    const result = await request(
-      Platform.select({
-        android: PERMISSIONS.ANDROID.RECORD_AUDIO,
-      }),
-    );
-
-    if (result === 'granted') {
-      console.log('마이크 권한이 허용되었습니다.');
-    } else {
-      console.log('마이크 권한이 거부되었습니다.');
-    }
-  };
-
   useEffect(() => {
     askPermission();
-    // startListenerTapped();
-    checkAndRequestPermission();
+    startListenerTapped();
   }, []);
+  const startListenerTapped = () => {
+    const callDetector = new CallDetectorManager(
+      event => {
+        if (event === 'Disconnected') {
+          // 전화가 끊어지면 File 컴포넌트의 기능을 호출
+          checkAndRequestPermission();
+        }
+      },
+      true,
+      () => {},
+      {
+        title: 'Phone State Permission',
+        message:
+          '이 앱은 전화 상태에 접근해야 하므로 전화가 수신되거나 전화 중일 때 반응하거나 적응하기 위해 권한이 필요합니다.',
+      },
+    );
+  };
 
   const checkAndRequestPermission = async () => {
     const status = await check(PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE);
@@ -65,10 +67,10 @@ const Navigation = () => {
     } else {
       const result = await request(PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE);
       if (result === RESULTS.GRANTED) {
-        console.log('권한이 승인되었습니다.');
+        console.log('저장소 권한이 승인되었습니다.');
         getMostRecentFile();
       } else {
-        console.log('권한이 거부되었습니다.');
+        console.log('저장소 권한이 거부되었습니다.');
       }
     }
   };
@@ -77,7 +79,6 @@ const Navigation = () => {
     try {
       const files = await RNFetchBlob.fs.ls(directoryPath);
       if (files.length > 0) {
-        console.log('파일들: ' + files);
         files.sort(async (a, b) => {
           const statA = await RNFetchBlob.fs.stat(`${directoryPath}${a}`);
           const statB = await RNFetchBlob.fs.stat(`${directoryPath}${b}`);
@@ -85,7 +86,7 @@ const Navigation = () => {
         });
 
         const mostRecentFile = files[0];
-        setMostRecentFile(directoryPath + mostRecentFile); // 가장 최신 파일 경로!! 설정
+        setMostRecentFile(mostRecentFile); // 가장 최신 파일 경로!! 설정
       } else {
         console.log('디렉토리에 파일이 없습니다.');
       }
@@ -94,6 +95,46 @@ const Navigation = () => {
     }
   };
 
+  useEffect(() => {
+    if (mostRecentFile) {
+      sendFile();
+    }
+  }, [mostRecentFile]);
+
+  const sendFile = async () => {
+    console.log('file ' + mostRecentFile);
+    if (mostRecentFile) {
+      const formData = new FormData();
+      formData.append('file', {
+        uri: 'file://' + directoryPath + mostRecentFile,
+        type: 'audio/x-m4a',
+        name: mostRecentFile,
+      });
+
+      fetch(`${url}/stt`, {
+        method: 'POST',
+        body: formData,
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+        .then(response => {
+          setMostRecentFile(null);
+          if (response.ok) {
+            return response.json();
+          }
+          return Error('업로드 실패');
+        })
+        .then(data => {
+          console.log('업로드 성공', data);
+        })
+        .catch(error => {
+          console.error('업로드 실패', error);
+        });
+    } else {
+      console.log('mostRecentFile이 없어서 업로드를 수행할 파일이 없습니다.');
+    }
+  };
   const askPermission = async () => {
     try {
       const permissions = await PermissionsAndroid.requestMultiple([
@@ -104,43 +145,6 @@ const Navigation = () => {
     } catch (err) {
       console.warn(err);
     }
-  };
-
-  const startListenerTapped = () => {
-    const callDetector = new CallDetectorManager(
-      (event, num) => {
-        console.log(event + ' ' + num);
-        if (event === 'Disconnected') {
-          // 통화 종료 시
-          // setIncoming(false);
-          // setNumber(null);
-
-          // 전화가 끊어지면 File 컴포넌트의 기능을 호출
-          checkAndRequestPermission();
-        }
-        //전화 끊었을때만 파악하면 되긴함! 그래서 일단 주석
-        // else if (event === 'Incoming') {
-        //   // 수신 전화 시
-        //   setIncoming(true);
-        //   setNumber(num);
-        // } else if (event === 'Offhook') {
-        //   // 통화 중 (Off-hook) 상태
-        //   setIncoming(true);
-        //   setNumber(num);
-        // } else if (event === 'Missed') {
-        //   // 부재 중 전화 시
-        //   setIncoming(false);
-        //   setNumber(null);
-        // }
-      },
-      true,
-      () => {},
-      {
-        title: 'Phone State Permission',
-        message:
-          '이 앱은 전화 상태에 접근해야 하므로 전화가 수신되거나 전화 중일 때 반응하거나 적응하기 위해 권한이 필요합니다.',
-      },
-    );
   };
 
   return (
